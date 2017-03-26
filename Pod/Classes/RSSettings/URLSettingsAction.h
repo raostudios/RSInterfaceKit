92d87ded238e190e50daabeaeabe365bad53fbae
Venkat S Rao <vrao423@gmail.com>
2017-03-26 17:07:02 -0400

    add url action

---
+++ Pod/Classes/RSSettings/URLSettingsAction.h
@@ -0,0 +1,15 @@
+//
+//  URLSettingsAction.h
+//  Pods
+//
+//  Created by Venkat Rao on 3/26/17.
+//
+//
+
+#import "SettingsAction.h"
+
+@interface URLSettingsAction : SettingsAction
+
+@property (strong, nonatomic) NSString *urlString;
+
+@end
---
+++ Pod/Classes/RSSettings/URLSettingsAction.m
@@ -0,0 +1,19 @@
+//
+//  URLSettingsAction.m
+//  Pods
+//
+//  Created by Venkat Rao on 3/26/17.
+//
+//
+
+#import "URLSettingsAction.h"
+
+@implementation URLSettingsAction
+
+-(void (^)(void))action {
+    return ^void(void) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:self.urlString]];
+    };
+}
+
+@end
